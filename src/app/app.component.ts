4ba30f2b6d75ab94a75352c7e8b844af997a68d0
Sergiio <sergiiomgdaw@gmail.com>
2025-10-20 21:12:38 +0200

    seccion novedades prototipo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderKioscoComponent } from "./header-kiosco/header-kiosco.component";
+import { NovedadesKioscoComponent } from './novedades-kiosco/novedades-kiosco.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderKioscoComponent],
+  imports: [RouterOutlet, HeaderKioscoComponent, NovedadesKioscoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
