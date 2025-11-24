ee4ec7a415c38dda3fb069a5746d41084e150a29
Sergiio <sergiiomgdaw@gmail.com>
2025-11-24 16:48:38 +0100

    Menu navegacion responsive

--- src/app/header-kiosco/header-kiosco.component.ts
+++ src/app/header-kiosco/header-kiosco.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header-kiosco.component.css'
 })
 export class HeaderKioscoComponent {
-
+  menuOpen = false;
+  
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    }
 }
