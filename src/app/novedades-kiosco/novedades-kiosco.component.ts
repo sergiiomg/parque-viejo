bf138b4ea118ab6f48dc4e26aef8fd6e3859198c
Sergiio <sergiiomgdaw@gmail.com>
2026-03-19 19:28:09 +0100

    Cambio de imagenes

--- src/app/novedades-kiosco/novedades-kiosco.component.ts
+++ src/app/novedades-kiosco/novedades-kiosco.component.ts
@@ -28,34 +28,52 @@ import  { CarruselComponent} from '../carrusel/carrusel.component';
 })
 export class NovedadesKioscoComponent {
 novedades = [
-    
+  {
+      imagenes: [
+        'assets/cromos (1).jpeg',
+        'assets/cromos (2).jpeg',
+        'assets/cromos (3).jpeg',
+        'assets/cromos (4).jpeg',
+      ],
+      titulo: 'Cromos Adrenalyn 2025-2026',
+      visible: false,
+      tipo: 'carrusel',
+      descripcion: ''
+    },
+  {
+      imagen: 'assets/revista-febrero (2).png',
+      titulo: 'Revista Woman',
+      visible: false,
+      tipo: 'normal',
+      descripcion: 'La revista Woman (6,50€) del mes de febrero ya está a la venta! Como regalo tenemos una agenda 2026 con 12 ilustraciones de Jorge Arévalo. Viene además con una mini talla de la máscara de pestañas Bad dal Bang de Benefit.'
+    },
+  {
+      imagen: 'assets/revista-febrero (3).png',
+      titulo: 'Revista Telva',
+      visible: false,
+      tipo: 'normal',
+      descripcion: 'Ya está aquí la revista Telva del mes de febrero! Por el precio de 6,50€ tenemos de regalo este mes el contorno de ojos de Alma Secret en tamaño de 10ml.'
+    },
+  {
+      imagen: 'assets/revista-febrero (1).png',
+      titulo: 'Revista Harper´s Bazar',
+      visible: false,
+      tipo: 'normal',
+      descripcion: 'Este mes la revista Harper´s Bazar cuesta 5,95€ y de regalo trae Lip Boom o Intense Kisses de MUA Make Up.'
+    },
+    {
+      imagen: 'assets/elle.jpg',
+      titulo: 'Revista Elle',
+      visible: false,
+      tipo: 'normal',
+      descripcion: 'En la revista grande de ELLE por 6,50€ traerá unos saquitos de la Fundación Aladina y una crema hidratante corporal con esferas de vaquer, en formato 250ml.'
+    },
     {
       imagen: 'assets/adrenalyn.png',
       titulo: 'Adrenalyn',
       visible: false,
       tipo: 'normal',
       descripcion: 'Mañana jueves 8 llegan los cromos de Adrenalyn. Y el viernes 9 llegará el álbum. ¡Reserva el tuyo ya!'
-    },
-    {
-      imagen: 'assets/bandeja-chuches.jpeg',
-      titulo: 'Bandeja de golosinas',
-      visible: false,
-      tipo: 'normal'
-    },
-    {
-      imagenes: [
-        'assets/revoltijos-neceser.jpeg',
-        'assets/revoltijos-neceser2.jpeg'
-      ],
-      titulo: 'Revoltijos neceser',
-      visible: false,
-      tipo: 'carrusel'
-    },
-    {
-      imagen: 'assets/bolsa-papanoel.jpeg',
-      titulo: 'Revoltijos personalizados',
-      visible: false,
-      tipo: 'normal'
     },
     {
       imagenes: [
@@ -71,12 +89,7 @@ novedades = [
       tipo: 'carrusel',
       descripcion: 'Han llegado las revistas mensuales con regalo de agenda 2026 ¡No te quedes sin la tuya!'
     },
-    {
-      imagen: 'assets/bolsas-navidad.jpeg',
-      titulo: 'Revoltijos navideños',
-      visible: false,
-      tipo: 'normal'
-    },
+    
   ];
   @ViewChildren('cardRef') cardElements!: QueryList<ElementRef>;
 
